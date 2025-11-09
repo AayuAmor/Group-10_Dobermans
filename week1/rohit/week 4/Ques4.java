0e4e3991b7caac3b8e9ae9528d1303d0e60b51c4
rohitthakur722 <rohitthakur95959@gmail.com>
2025-11-09 21:00:57 +0545

    Adding task of week 4

---
+++ week1/rohit/week 4/Ques4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Ques4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 1; i<=10;i++){
+            System.out.print("Enter an Interger:");
+            int num = sc.nextInt();
+            if (num%2==0 & num>20){
+                System.out.println(num);
+            }
+        }
+        sc.close();
+    }
+
+}
